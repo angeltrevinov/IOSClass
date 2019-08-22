89040461be2bd9ddc93f73fa3282a14c7517b187
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-08-22 14:41:11 -0500

    Adding Sliders

---
+++ Slider/Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Angel Trevino on 8/19/19.
+//  Copyright © 2019 Angel Odiel Treviño Villanueva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
