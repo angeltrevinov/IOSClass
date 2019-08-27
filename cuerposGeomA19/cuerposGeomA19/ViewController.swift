de7b18d77658acadf68f459cc7ae4ef95bc9e66f
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-08-27 10:14:31 -0500

    Adding HW

---
+++ cuerposGeomA19/cuerposGeomA19/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  cuerposGeomA19
+//
+//  Created by Yolanda Martinez on 8/25/19.
+//  Copyright © 2019 com.itesm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imgFigureHome: UIImageView!
+    @IBOutlet weak var lbFHome: UILabel!
+    @IBOutlet weak var lbSHome: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindEsfera(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func unwindPrisma(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+}
+
